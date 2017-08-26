012b1a480808d16770979d316b1f836569b49fc2
davearock <davidrock@Davids-MacBook-Pro.local>
2017-08-26 14:02:04 -0500

    finished ch2

--- ch2.py
+++ ch2.py
@@ -125,4 +125,233 @@ plt.show()
 
 
 
+class AdalineGD(object):
+    """ ADAptive LInear NEuron classifier.
+    
+    Parameters
+    -----------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter: int
+        number of passes over the training data
+    
+    Attributes
+    -----------
+    w_ : 1d array
+        weights after fitting.
+    errors_ : list
+        number of misclassifications in each epoch
+    
+    """
+    def __init__(self, eta = 0.01, n_iter = 50):
+        self.eta = eta
+        self.n_iter = n_iter
+        
+    def fit(self, X, y):
+        """ Fit training data
+        
+        Parameters
+        -----------
+        X : {array-life}, shape = [n_samples, n_features]
+            Training vectors,
+            where n_samples is the number of samples and 
+            n_features is the number of features.
+        y : array, shape = [n_samples]
+            Target values
+        
+        Returns
+        -----------
+        self: object
+        
+        """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+        
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+    
+    def net_input(self, X):
+        """calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def activation(self, X):
+        """compute linear activation"""
+        return self.net_input(X)
+    
+    def predict(self, X):
+        """return class label after each unit step"""
+        return np.where(self.activation(X) >= 0, 1, -1)
+
+
+
+
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (8, 4))
+ada1 = AdalineGD(n_iter = 10, eta = 0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1),
+          np.log10(ada1.cost_),
+          marker = 'o')
+ax[0].set_xlabel('epochs')
+ax[0].set_ylabel('log(sum-squared-error)')
+ax[0].set_title('Adaline - learning rate 0.01')
+ada2 = AdalineGD(n_iter = 10, eta = 0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1),
+          np.log10(ada2.cost_),
+          marker = 'o')
+ax[1].set_xlabel('epochs')
+ax[1].set_ylabel('log(sum-squared-error)')
+ax[1].set_title('Adaline - learning rate 0.0001')
+plt.show()
+
+
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = AdalineGD(n_iter = 15, eta = 0.01)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - gradient descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc = 'upper left')
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker = 'o')
+plt.xlabel('epochs')
+plt.ylabel('sum-squared-error')
+plt.show()
+
+
+from numpy.random import seed
+class AdalineSGD(object):
+    """ ADAptive LInear NEuron classifier.
+    
+    Parameters
+    -----------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter: int
+        number of passes over the training data
+    
+    Attributes
+    -----------
+    w_ : 1d array
+        weights after fitting.
+    errors_ : list
+        number of misclassifications in each epoch
+    shuffle : bool (default = True)
+        shuffles training after every epoch
+        if True to prevent cycles
+    random_state : int (default: None)
+        set random state for shuffling
+        and initiating the weights
+    
+    """
+    def __init__(self, eta = 0.01, n_iter = 10,
+                 shuffle = True, random_state = None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+        
+    def fit(self, X, y):
+        """ Fit training data
+        
+        Parameters
+        -----------
+        X : {array-life}, shape = [n_samples, n_features]
+            Training vectors,
+            where n_samples is the number of samples and 
+            n_features is the number of features.
+        y : array, shape = [n_samples]
+            Target values
+        
+        Returns
+        -----------
+        self: object
+        
+        """
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+        return self
+    
+    def partial_fit(self, X, y):
+        """fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+    
+    def _shuffle(self, X, y):
+        """ Shuffle training data"""
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+    
+    def _initialize_weights(self, m):
+        """ initialize weights to zero"""
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+    
+    def _update_weights(self, xi, target):
+        """ apply Adaline learning rule to update the weights """
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        return cost  
+    
+    def net_input(self, X):
+        """calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def activation(self, X):
+        """compute linear activation"""
+        return self.net_input(X)
+    
+    def predict(self, X):
+        """return class label after each unit step"""
+        return np.where(self.activation(X) >= 0, 1, -1)
+
+
+
+ada = AdalineSGD(n_iter = 15, eta = 0.01, random_state = 1)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier = ada)
+plt.title('Adaline - Stochastic Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc = 'upper left')
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker = 'o')
+plt.xlabel('epochs')
+plt.ylabel('average cost')
+plt.show()
+
+
+
+
+
+
+
+
 
